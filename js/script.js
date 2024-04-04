bb7a4ed30fedf15d019da178267d9f386082dfa5
Slava.Gvsh <slava.gvsh@gmail.com>
2024-04-04 16:05:34 +0300

    first commit3

--- js/script.js
+++ js/script.js
@@ -2,20 +2,25 @@ $(document).ready(function () {
   $(".burger__menu").click(function (event) {
     $(".nav, .burger__menu").toggleClass("active");
     $("body").toggleClass("lock");
+    
   });
+  $(".nav__item-drop").click(function (event) {
+   $(".nav__link-drop, .dropdown__list").toggleClass("active");
+   
+ });
   
 //   $(".nav__item-drop").click(function () {
 //    $(this).find(".dropdown__list").slideToggle(200);
 //  });
 // Проверяем ширину экрана
-var screenWidth = $(window).width();
+// var screenWidth = $(window).width();
 
-// Если ширина экрана меньше 768px (или другого значения для мобильных устройств)
-if (screenWidth < 768) {
-  // Применяем slideToggle() к элементу
-  $('.nav__item-drop').click(function(){
-    $(this).find(".dropdown__list").slideToggle(200);
-  });
-}
+// // Если ширина экрана меньше 768px (или другого значения для мобильных устройств)
+// if (screenWidth < 768) {
+//   // Применяем slideToggle() к элементу
+//   $('.nav__item-drop').click(function(){
+//     $(this).find(".dropdown__list").slideToggle(200);
+//   });
+// }
 });
 
